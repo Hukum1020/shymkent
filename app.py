393553e802776fb2351336c144cbad2951c60244
Hukum1020 <nikitasicily22@gmail.com>
2025-03-22 15:48:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ except Exception as e:
 # ------------------------------
 # Настройка SMTP (Gmail)
 # ------------------------------
-SMTP_SERVER = "smtp.gmail.com"
+SMTP_SERVER = "smtp-relay.brevo.com"
 SMTP_PORT = 587
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
@@ -50,15 +50,22 @@ SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 if not SMTP_USER or not SMTP_PASSWORD:
     raise ValueError("❌ Ошибка: SMTP_USER или SMTP_PASSWORD не найдены!")
 
-def send_email(email, qr_filename, language):
+def send_email(email, qr_filename, language, name=None):
     try:
-        subject_ru = f"Ваш персональный QR-код #{random.randint(1000, 9999)}"
-        subject_kz = f"Сіздің жеке QR-кодыңыз #{random.randint(1000, 9999)}"
+        random_code = random.randint(1000, 9999)
+        if name:
+            subject_ru = f"{name}, ваш персональный QR-код #{random_code}"
+            subject_kz = f"{name}, сіздің жеке QR-кодыңыз #{random_code}"
+        else:
+            subject_ru = f"Ваш персональный QR-код #{random_code}"
+            subject_kz = f"Сіздің жеке QR-кодыңыз #{random_code}"
+
         msg = EmailMessage()
-        msg["From"] = SMTP_USER
+        msg["From"] = "noreply@biecosystem.kz"
         msg["To"] = email
         msg["Subject"] = subject_ru if language == "ru" else subject_kz
-        msg.set_type("multipart/related")  # Оставляем для встраивания QR-кода
+        msg.set_type("multipart/related")
+
 
         # Загружаем HTML-шаблон
         template_filename = f"shym{language}.html"
@@ -127,7 +134,7 @@ def process_new_guests():
             qr = qrcode.make(qr_data)
             qr.save(qr_filename)
             
-            if send_email(email, qr_filename, language):
+            if send_email(email, qr_filename, language, name=name):
                 sheet.update_cell(i+1, 9, "Done")
     except Exception as e:
         print(f"[Ошибка] при обработке гостей: {e}")
