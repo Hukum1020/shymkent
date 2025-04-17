ec86a1b11bc04f923c10e65d2d085ee49284527c
Hukum1020 <nikitasicily22@gmail.com>
2025-04-17 21:19:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -50,15 +50,20 @@ SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 if not SMTP_USER or not SMTP_PASSWORD:
     raise ValueError("❌ Ошибка: SMTP_USER или SMTP_PASSWORD не найдены!")
 
-def send_email(email, qr_filename, language, name=None, row_index=None, sheet=None):
+def send_email(email, qr_filename, language, name=None):
     try:
         random_code = random.randint(1000, 9999)
-        subject = "Выигрывайте призы на BI Ecosystem в Шымкенте - уже завтра!"
+        if name:
+            subject_ru = "Выигрывайте призы на BI Ecosystem в Шымкенте - уже завтра!"
+            subject_kz = "Ертең Шымкентте өтетін BI Ecosystem шарасында жүлде ұтып алыңыз!"
+        else:
+            subject_ru = "Выигрывайте призы на BI Ecosystem в Шымкенте - уже завтра!"
+            subject_kz = "Ертең Шымкентте өтетін BI Ecosystem шарасында жүлде ұтып алыңыз!"
 
         msg = EmailMessage()
         msg["From"] = "noreply@biecosystem.kz"
         msg["To"] = email
-        msg["Subject"] = subject
+        msg["Subject"] = subject_ru if language == "ru" else subject_kz
         msg.set_type("multipart/related")
 
         template_filename = f"shym{language}.html"
